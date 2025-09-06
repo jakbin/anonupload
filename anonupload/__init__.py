45f8a8cc3d8a83332e7ff4d12043ca5e5b2099a2
jakbin <jakbin4747@gmail.com>
2025-09-06 10:09:03 +0530

    bump version to 1.4.3 and update exports in __all__

--- anonupload/__init__.py
+++ anonupload/__init__.py
@@ -1,3 +1,5 @@
-__version__= "1.4.2"
+__version__= "1.4.3"
 
-from .main import download, downloads, upload, changefile_and_upload
+from .main import download, downloads, upload, changefile_and_upload, detect_filename, download_and_upload, multi_upload
+
+__all__ = ["download", "downloads", "upload", "changefile_and_upload", "detect_filename", "download_and_upload", "multi_upload"]
